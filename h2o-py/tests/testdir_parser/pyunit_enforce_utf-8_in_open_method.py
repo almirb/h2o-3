3d4ce669193172e7718c2de472891e3538a46c21
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2022-01-12 17:30:34 +0100

    clean up

--- h2o-py/tests/testdir_parser/pyunit_enforce_utf-8_in_open_method.py
+++ h2o-py/tests/testdir_parser/pyunit_enforce_utf-8_in_open_method.py
@@ -6,7 +6,7 @@ from tests import pyunit_utils
 import pandas as pd
 import locale
 
-def encofrce_utf8_encoding():
+def enforce_utf8_encoding():
     orig_locale = locale.getlocale()
     all_rows = pd.read_csv(pyunit_utils.locate("smalldata/gbm_test/titanic.csv"))
     all_rows.at[0,0] = "�"
@@ -14,15 +14,14 @@ def encofrce_utf8_encoding():
         locale.setlocale(locale.LC_ALL, 'en_US.ISO8859-1')
         # this reproduces the encoding error when certain codec can't encode certain character 
         h2o.H2OFrame(all_rows)
-        locale.setlocale(locale.LC_ALL, orig_locale)
     except locale.Error: # in run in dev-python-3.7 there is not en_US.ISO8859-1 available, but there is POSIX which also reproduces:
         locale.setlocale(locale.LC_ALL, 'POSIX')
         h2o.H2OFrame(all_rows)
+    finally:
         locale.setlocale(locale.LC_ALL, orig_locale)
-        
     
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(encofrce_utf8_encoding)
+    pyunit_utils.standalone_test(enforce_utf8_encoding)
 else:
-    encofrce_utf8_encoding()
+    enforce_utf8_encoding()
 
